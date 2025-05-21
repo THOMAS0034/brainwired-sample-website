d7eebb72d0d8cb74e6b013e6acfebfd06e9aa227
THOMAS0034 <107304775+THOMAS0034@users.noreply.github.com>
2025-05-21 21:36:02 +0530

    update 1

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Home.css'
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './Home.css';
 
 export const Home = () => {
+    useEffect(() => {
+    AOS.init({ duration: 1000 });
+  },[]);
   return (
-    <div>
-        <img className='homeimage1' src="/imageset/simon-maisch-mTujepy0ers-unsplash.jpg" alt="" />
-        <p className='p1'>Brainwired is an agritech startup based out of India,
+    <div className='view'>
+        <div>
+        <img className='homeimage1'   src="/imageset/simon-maisch-mTujepy0ers-unsplash.jpg" alt="" />
+        <p className='p1' data-aos="fade-up" data-aos-delay="200">Brainwired is an agritech startup based out of India,
             which has developed a livestock health monitoring, and tracking system named WeSTOCK.
             Our motive is to empower the lifestyle of all livestock farmers with high end technology
             at an affordable rate. Our team has a passion for making things with real value. This has
@@ -13,6 +21,17 @@ export const Home = () => {
             creating a brand through strategy, story-telling, products, and integrated experiences on web,
              mobile, and in the world. So that we become a household name among Indian farmers becoming a
               symbol of trust and quality</p>
+        </div>
+        <div className='block square' data-aos="fade-up" data-aos-delay="200">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Neque, ratione asperiores placeat ut culpa quidem ducimus officiis vitae similique iusto totam corporis iure
+            earum natus cum consequuntur sint sapiente. Omnis!
+        </div>
+        <div className='block rect' data-aos="fade-up" data-aos-delay="300">Block 2</div>
+        <div className='block square' data-aos="fade-up" data-aos-delay="40">Block 3</div>
+        <div className='block rect' data-aos="fade-up" data-aos-delay="500">Block 4</div>
+        <div className='block square' data-aos="fade-up" data-aos-delay="600">Block 5</div>
+        <div className='block rect' data-aos="fade-up" data-aos-delay="700">Block 6</div>
+        <div className='block square' data-aos="fade-up" data-aos-delay="800">Block 7</div>
     </div>
     
   )
