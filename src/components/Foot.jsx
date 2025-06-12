4974b5216f28918c0b3a94ebef170f87e7a96ca6
THOMAS0034 <107304775+THOMAS0034@users.noreply.github.com>
2025-06-12 11:50:12 +0530

    footer

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -42,21 +42,21 @@ export const Foot = () => {
           <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
             <h6 className="text-uppercase fw-bold">Products</h6>
             <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} />
-            <p><Link to="/westock" className="text-white">Westock</Link></p>
-            <p><a href="#!" className="text-white">Togo</a></p>   
+            <p><Link to="/westock" className="text-grey foot-link">Westock</Link></p>
+            <p><a href="#!" className="text-grey foot-link">Togo</a></p>   
           </div>
 
           <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <h6 className="text-uppercase fw-bold">Useful links</h6>
+            <h6 className="text-uppercase fw-bold foot-link-head">Useful links</h6>
             <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} />
-            <p><Link to="/career" className="text-white">Career</Link></p>
-            <p><Link to="/team" className="text-white">Team</Link></p>
-            <p><Link to="/aboutus" className="text-white">About us</Link></p>
-            <p><Link to="/blog" className="text-white">Blog</Link></p>
+            <p><Link to="/career" className="text-grey foot-link">Career</Link></p>
+            <p><Link to="/team" className="text-grey foot-link">Team</Link></p>
+            <p><Link to="/aboutus" className="text-grey foot-link">About us</Link></p>
+            <p><Link to="/blog" className="text-grey foot-link">Blog</Link></p>
           </div>
 
           <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <h6 className="text-uppercase fw-bold">Contact</h6>
+            <h6 className="text-uppercase fw-bold foot-link-head">Contact</h6>
             <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} />
             <p><i className="fas fa-home mr-3"></i> 1st Floor, Maker Village, Kerala Technology Innovation Zone, Kinfra Hi-Tech Park Main Rd, North Kalamassery, Kochi, Kerala 683503</p>
             <p><i className="fas fa-envelope mr-3"></i> info@brainwired.in</p>
