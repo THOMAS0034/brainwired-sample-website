ca6d758cee1dda896f1a46f38f7edbfb6b44ef33
THOMAS0034 <107304775+THOMAS0034@users.noreply.github.com>
2025-06-17 13:59:16 +0530

    togoservice

--- src/components/togocomp/Service.jsx
+++ src/components/togocomp/Service.jsx
@@ -1,68 +1,90 @@
 import React from 'react'
 import './Service.css'
-import AOS from 'aos';
-import { useEffect } from 'react';
-import 'aos/dist/aos.css';
+import { rgba } from 'framer-motion'
 
 export const Service = () => {
-        useEffect(() => {
-            AOS.init({ duration: 1000 });
-            setTimeout(() => {
-              AOS.refresh();
-            }, 500);
-          }, []);
   return (
     <div>
-        <h1>What we provide</h1>
-<div className="togo-features">
-  <h1 className='togo-featurehead' data-aos='zoom-in' data-aos-delay='400' style={{color:'rgb(234, 144, 33)'}}>Online Vet Consultations</h1>
-  <div className="togo-flex-right">
-    <img className='togo-feature-image' src='./imageset/togo_health.png'/>
-    <p className='togo-feature-content' data-aos='fade-left' data-aos-delay='500'>
-      TOGO connects pet owners with licensed veterinarians for real-time online consultations. Whether it’s a health concern, a behavioral issue, or an emergency, expert advice is just a click away—ensuring your pet receives timely and accurate care from the comfort of home.
-    </p>
-  </div>
+        <h1 className='paltoo-header' >Introducing Paltoo collar</h1>
+        <div className="paltooheader">
+            <img className='paltoo-img' src="./imageset/paltoo-collar.avif" alt="" />
+            <div className="paltoo-text">
+                <h1 className='paltoo-text-h1'>PALTOO COLLAR</h1>
+                <h3 className='paltoo-text-h3'>  The Smart Collar That Keeps Your Pet Safe & Healthy</h3>
+            <p className='paltoo-def'>Paltoo is an AI-powered smart collar that continuously monitor’s your pet’s vital signs, activity levels and behaviour patterns. Get real-time alerts and insights about your pet’s health directly in your smartphone</p>
+            </div>
+        </div>
 
-  <h1 className='togo-featurehead' data-aos='zoom-in' data-aos-delay='400' style={{color:'rgb(225, 38, 38)'}}>Medicine & Supplements Delivery</h1>
-  <div className="togo-flex-left">
-    <img className='togo-feature-image' src='./imageset/togo_location.png'/>
-    <p className='togo-feature-content' data-aos='fade-right' data-aos-delay='500'>
-      TOGO makes pet healthcare easier by enabling doorstep delivery of prescribed medications, supplements, and essential treatments. Easily order your vet-prescribed medicines directly through the app and never miss a dose again.
-    </p>
-  </div>
+        <h1 className='explore-feat'>Explore The Features of Paltoo</h1>
 
-  <h1 className='togo-featurehead' data-aos='zoom-in' data-aos-delay='400' style={{color:'rgb(41, 125, 209)'}}>Personalized Nutrition Guidance</h1>
-  <div className="togo-flex-right">
-    <img className='togo-feature-image' src='./imageset/togo_activity.png'/>
-    <p className='togo-feature-content' data-aos='fade-left' data-aos-delay='500'>
-      Every pet is unique—and so are their dietary needs. TOGO provides tailored nutritional advice based on your pet’s breed, age, weight, and medical history. Get vet-backed diet plans and food suggestions to ensure optimal health and longevity.
-    </p>
-  </div>
 
-  <h1 className='togo-featurehead' data-aos='zoom-in' data-aos-delay='400' style={{color:'rgb(175, 88, 179)'}}> On-Demand Grooming Services</h1>
-  <div className="togo-flex-left">
-    <img className='togo-feature-image' src='./imageset/togo_behavior.png'/>
-    <p className='togo-feature-content' data-aos='fade-right' data-aos-delay='500'>
-      Keep your pets clean, healthy, and stylish with TOGO’s on-demand grooming feature. Book professional groomers for at-home grooming appointments that include bathing, nail clipping, hair trimming, and more.
-    </p>
-  </div>
+        <div className="service-container">
+            <div className="service-card">
+                <img className='service-card-img' src="./imageset/activity.png" alt="" />
+                <h1 className='service-card-head'>Vet Support</h1>
+                <p className='service-card-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore neque labore ea temporibus enim explicabo deserunt perferendis ad, minus odit?</p>
+            </div>
 
-  <h1 className='togo-featurehead' data-aos='zoom-in' data-aos-delay='400' style={{color:'rgb(100, 118, 61)'}}>Home-Based Medical Testing</h1>
-  <div className="togo-flex-right">
-    <img className='togo-feature-image' src='./imageset/togo_environment.png'/>
-    <p className='togo-feature-content' data-aos='fade-left' data-aos-delay='500'>
-        TOGO allows pet owners to schedule essential medical tests like blood tests, stool samples, or allergy checks right from their home. Certified professionals collect samples and upload results directly into your TOGO health dashboard.
-    </p>
-  </div>
+            <div className="service-card">  
+                <img className='service-card-img' src="./imageset/activity.png" alt="" />
+                <h1 className='service-card-head'>Vet Support</h1>
+                <p className='service-card-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore neque labore ea temporibus enim explicabo deserunt perferendis ad, minus odit?</p>
+            </div>
 
-  <h1 className='togo-featurehead' data-aos='zoom-in' data-aos-delay='400' style={{color:'rgb(255, 140, 0)'}}>Pet Marketplace & E-Commerce</h1>
-  <div className="togo-flex-left">
-    <img className='togo-feature-image' src='./imageset/togo_vet.png'/>
-    <p className='togo-feature-content' data-aos='fade-right' data-aos-delay='500'>
-      Explore TOGO’s all-in-one marketplace where you can buy and sell pets or accessories, and access a full-fledged e-commerce section to shop for food, toys, accessories, apparel, and more—curated by trusted pet brands.
-    </p>
-  </div>
-</div>
-</div>
+            <div className="service-card">
+                <img className='service-card-img' src="./imageset/activity.png" alt="" />
+                <h1 className='service-card-head'>Vet Support</h1>
+                <p className='service-card-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore neque labore ea temporibus enim explicabo deserunt perferendis ad, minus odit?</p>
+            </div>
+
+            <div className="service-card">
+                <img className='service-card-img' src="./imageset/activity.png" alt="" />
+                <h1 className='service-card-head'>Vet Support</h1>
+                <p className='service-card-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore neque labore ea temporibus enim explicabo deserunt perferendis ad, minus odit?</p>
+            </div>
+
+            <div className="service-card">
+                <img className='service-card-img' src="./imageset/activity.png" alt="" />
+                <h1 className='service-card-head'>Vet Support</h1>
+                <p className='service-card-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore neque labore ea temporibus enim explicabo deserunt perferendis ad, minus odit?</p>
+            </div>
+
+            <div className="service-card">
+                <img className='service-card-img' src="./imageset/activity.png" alt="" />
+                <h1 className='service-card-head'>Vet Support</h1>
+                <p className='service-card-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore neque labore ea temporibus enim explicabo deserunt perferendis ad, minus odit?</p>
+            </div>
+        </div>
+        <button className='buy-paltoo' >BUY PALTOO</button>
+        <h1 className='togo-header' >Discover our TOGO Application</h1>
+        <p className='togo-def'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae velit exercitationem, asperiores non consectetur aliquid. Sed quisquam error, facilis animi eveniet ducimus fuga reprehenderit rerum, corporis tempore possimus, rem atque!</p>
+        <div className="togo-learn-container">
+            <img className='togo-learn-img' src="./imageset/togo-img.avif" alt="" />
+            <div className="togo-learn-text">
+                <h1 className='togo-feature-header'>Features of TOGO</h1>
+                <div className="togo-feat-container">
+                    <div className="togo-feat-card">
+                        <h3 className='togo-feat-head'>Pet Profile</h3>
+                        <p className='togo-feat-p'>Users can manage pet profile for each pets</p>
+                    </div>
+                    <div className="togo-feat-card">
+                        <h3 className='togo-feat-head'>Pet Profile</h3>
+                        <p className='togo-feat-p'>Users can manage pet profile for each pets</p>
+                    </div>
+
+                    <div className="togo-feat-card">
+                        <h3 className='togo-feat-head'>Pet Profile</h3>
+                        <p className='togo-feat-p'>Users can manage pet profile for each pets</p>
+                    </div>
+
+                    <div className="togo-feat-card">
+                        <h3 className='togo-feat-head'>Pet Profile</h3>
+                        <p className='togo-feat-p'>Users can manage pet profile for each pets</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
